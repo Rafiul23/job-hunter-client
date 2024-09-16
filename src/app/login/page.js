f1adc8a468570122025ccf49bc2ab381ef852a48
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-16 19:20:02 +0600

    added an image in login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,11 @@
-
+import Image from 'next/image';
+import loginImage from '../../assets/login.png';
 
 const Login = () => {
     return (
         <div className='py-10'>
            <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
-           <div>image</div>
+           <div><Image src={loginImage} alt='login image' width={500} height={500} /></div>
            <div>Form</div>
            </div>
         </div>
