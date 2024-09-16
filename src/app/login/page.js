1895c63701f5bcf426ee0998c1c43e14eb466710
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-16 21:02:57 +0600

    show and hide password is done

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,18 @@
+"use client";
 import Image from "next/image";
 import loginImage from "../../assets/login.png";
+import Link from "next/link";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
+
+    const [hidden, setHidden] = useState(true);
+
   return (
     <div className="py-10">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="w-full">
           <Image src={loginImage} alt="login image" width={500} height={500} />
         </div>
         <div className="card bg-base-100 w-full shadow-xl">
@@ -28,21 +35,26 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="password"
+                type={ hidden ? "password" : "text"}
                 placeholder="password"
                 className="input input-bordered"
                 required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
+              <p className='pt-6'>New to Job Hunter? Please, <Link href='/signup' className='text-[#033f63] font-bold'>Sign Up!</Link> </p>
             </div>
             <div className="form-control mt-6">
               <button className="btn bg-[#033f63] text-white">Login</button>
             </div>
           </form>
+
+          <div className="relative -top-[195px]">
+            <div className="absolute right-16">
+              <button onClick={() => setHidden(!hidden)}>
+                {hidden ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}
+              </button>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
