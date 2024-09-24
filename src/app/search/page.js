afe37da91b01dc2dd02e564feca57592e262798d
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-24 22:42:21 +0600

    refactored our code in search page

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,36 +1,38 @@
-'use client';
+"use client";
 import Image from "next/image";
-import corporateImage from '../../assets/globalisation-1014524_1280.png'
+import corporateImage from "../../assets/globalisation-1014524_1280.png";
 import JobCard from "@/Components/JobCard/JobCard";
 import { useState } from "react";
 
 const Search = () => {
-
   const [displayJobs, setDisplayJobs] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
-  const handleSearchJobs = async(e) =>{
-        e.preventDefault();
+  const handleSearchJobs = async (e) => {
+    e.preventDefault();
 
-        const searchContent = e.target.search.value;
+    const searchContent = e.target.search.value;
 
-        if(searchContent === ''){
-          return;
-        } else {
+    if (searchContent === "") {
+      return;
+    } else {
+      const res = await fetch(
+        `http://localhost:5000/search?title=${searchContent}`
+      );
 
-        const res = await fetch(`http://localhost:5000/search?title=${searchContent}`);
-        
-        const searchResult = await res.json(); 
-        if(searchResult.length > 0){
-          setDisplayJobs(searchResult);
-        } else {
-          setMessage('No result found!')
-        }
-        }
-  }
+      const searchResult = await res.json();
 
-    return (
-        <div className="py-10">
+      // console.log(searchResult);
+      if (searchResult.length === 0) {
+        setMessage("No result found!");
+      } else {
+        setDisplayJobs(searchResult);
+      }
+    }
+  };
+
+  return (
+    <div className="py-10">
       <Image
         src={corporateImage}
         className="w-full h-[250px]"
@@ -43,26 +45,32 @@ const Search = () => {
       <p className="py-2 text-center text-xl"> Search jobs by job title.</p>
       <div className="py-4 text-center">
         <form onSubmit={handleSearchJobs}>
-        <input
-          type="text"
-          placeholder="Type here"
-          name='search'
-          className="input input-bordered input-info w-full max-w-xs"
-        />
-        <button className='btn bg-[#033f63] text-white ml-2'>Search</button>
+          <input
+            type="text"
+            placeholder="Type here"
+            name="search"
+            className="input input-bordered input-info w-full max-w-xs"
+          />
+          <button className="btn bg-[#033f63] text-white ml-2">Search</button>
         </form>
       </div>
 
-     { displayJobs.length > 0 ? <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4'>
-            {
-                displayJobs?.map(job => <JobCard job={job} key={job._id}></JobCard>) 
-            }
+      {displayJobs.length === 0 ? 
+      
+      (
+        <div className="text-red-500 text-center font-semibold py-4 text-xl">
+          {message}
         </div>
-        :
-         <div className='text-red-500 text-center font-semibold py-4 text-xl'>{message}</div>
-        }
+      )
+      : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4">
+          {displayJobs?.map((job) => (
+            <JobCard job={job} key={job._id}></JobCard>
+          ))}
+        </div>
+      )  }
     </div>
-    );
+  );
 };
 
 export default Search;
