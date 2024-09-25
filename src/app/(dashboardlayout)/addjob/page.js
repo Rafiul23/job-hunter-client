b2110e76512d8ac2bf2cc356ae227a62669c9d35
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-25 22:42:59 +0600

    added input field for qualification and salary range

--- src/app/(dashboardlayout)/addjob/page.js
+++ src/app/(dashboardlayout)/addjob/page.js
@@ -9,14 +9,14 @@ const AddJob = ()=>{
         {/* input for company name */}
         <div className="form-control md:w-1/2 w-full">
           <label className="label">
-            <span className="label-text">Company Name:</span>
+            <span className="label-text font-bold">Company Name:</span>
           </label>
           <input type="text" placeholder="Company Name" name='company_name' className="input input-bordered" required />
         </div>
         {/* input for job title */}
         <div className="form-control md:w-1/2 w-full">
           <label className="label">
-            <span className="label-text">Job Title</span>
+            <span className="label-text font-bold">Job Title</span>
           </label>
           <input type="text" placeholder="Job Title" name='job_title' className="input input-bordered" required />
         </div>
@@ -27,16 +27,34 @@ const AddJob = ()=>{
         {/* input for location */}
         <div className="form-control md:w-1/2 w-full">
           <label className="label">
-            <span className="label-text">Location:</span>
+            <span className="label-text font-bold">Location:</span>
           </label>
           <input type="text" placeholder="Location" name='location' className="input input-bordered" required />
         </div>
         {/* input for experience */}
         <div className="form-control md:w-1/2 w-full">
           <label className="label">
-            <span className="label-text">Experience:</span>
+            <span className="label-text font-bold">Experience:</span>
           </label>
           <input type="text" placeholder="Experience" name='experience' className="input input-bordered" required />
+        </div>
+        </div>
+
+        <div className='flex md:flex-row flex-col gap-4'>
+
+        {/* input for qualifications */}
+        <div className="form-control md:w-1/2 w-full">
+          <label className="label">
+            <span className="label-text font-bold">Qualifications:</span>
+          </label>
+          <input type="text" placeholder="Qualifications" name='qualifications' className="input input-bordered" required />
+        </div>
+        {/* input for salary_range */}
+        <div className="form-control md:w-1/2 w-full">
+          <label className="label">
+            <span className="label-text font-bold">Salary Range:</span>
+          </label>
+          <input type="text" placeholder="Salary Range" name='salary_range' className="input input-bordered" required />
         </div>
         </div>
 
