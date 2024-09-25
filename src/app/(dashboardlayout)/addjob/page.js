a41131b321583b2dd4036e064511abdf42a2b7d5
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-25 22:23:13 +0600

    added input fields for company name and job title

--- src/app/(dashboardlayout)/addjob/page.js
+++ src/app/(dashboardlayout)/addjob/page.js
@@ -1,8 +1,24 @@
 
 const AddJob = ()=>{
     return (
-        <div>
-        this is add job page
+        <div className='py-5'>
+        <h2 className='text-3xl text-center font-bold mb-2 text-[#033f63]'>Add a Job</h2>
+        <form className='p-4 bg-base-200'>
+        <div className='flex md:flex-row flex-col gap-4'>
+        <div className="form-control md:w-1/2 w-full">
+          <label className="label">
+            <span className="label-text">Company Name:</span>
+          </label>
+          <input type="text" placeholder="Company Name" name='company_name' className="input input-bordered" required />
+        </div>
+        <div className="form-control md:w-1/2 w-full">
+          <label className="label">
+            <span className="label-text">Job Title</span>
+          </label>
+          <input type="text" placeholder="Job Title" name='job_title' className="input input-bordered" required />
+        </div>
+        </div>
+        </form>
         </div>
     )
 }
