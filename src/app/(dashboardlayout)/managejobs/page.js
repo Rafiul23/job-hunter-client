4cc6c8e4657b29eda06ce9eb49b58679fa8121db
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-27 21:32:19 +0600

    defining number of pages and jobs per page

--- src/app/(dashboardlayout)/managejobs/page.js
+++ src/app/(dashboardlayout)/managejobs/page.js
@@ -1,8 +1,29 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
 
 const ManageJobs = ()=>{
+    const [totalCount, setTotalCount] = useState([]);
+
+    axios.get('http://localhost:5000/jobsCount')
+    .then(res => setTotalCount(res.data));
+
+    const jobsPerPage = 12;
+    const {count} = totalCount;
+    const numberOfPages = Math.ceil(count / jobsPerPage);
+
+    const pages = [];
+    for(let i = 0; i < numberOfPages; i++ ){
+        pages.push(i);
+    }
+   
+
     return (
-        <div>
-            this is manage job page
+        <div className='py-5'>
+           <h2 className="text-3xl text-center font-bold mb-2 text-[#033f63]">
+        Manage Jobs
+           </h2>
+
         </div>
     )
 }
