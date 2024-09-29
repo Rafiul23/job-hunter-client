310f118b391e57e2641acc3041ebe802c30fd28d
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-29 11:24:51 +0600

    added input field for company name

--- src/app/(dashboardlayout)/managejobs/[id]/page.js
+++ src/app/(dashboardlayout)/managejobs/[id]/page.js
@@ -1,44 +1,57 @@
 "use client";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-const UpdateJobPage = ({params})=>{
+const UpdateJobPage = ({ params }) => {
+  const [job, setJob] = useState([]);
 
-    const [job, setJob] = useState([]);  
-    
-    axios.get(`http://localhost:5000/job/${params?.id}`)
-    .then(res => setJob(res.data))
+  axios
+    .get(`http://localhost:5000/job/${params?.id}`)
+    .then((res) => setJob(res.data));
 
-    const {
-        company_name,
-        job_title,
-        job_description,
-        location,
-        experience,
-        qualifications,
-        salary_range,
-        _id,
-        deadline,
-        category,
-        onsite_or_remote,
-        job_type,
-        employer_email,
-        job_post
-      } = job;
+  const {
+    company_name,
+    job_title,
+    job_description,
+    location,
+    experience,
+    qualifications,
+    salary_range,
+    _id,
+    deadline,
+    category,
+    onsite_or_remote,
+    job_type,
+    employer_email,
+    job_post,
+  } = job;
 
-    return (
-        <div className='py-5'>
-            <h2 className="text-3xl text-center font-bold mb-2 text-[#033f63]">
-            Update {job_title} Job 
-            </h2>
+  return (
+    <div className="py-5">
+      <h2 className="text-3xl text-center font-bold mb-2 text-[#033f63]">
+        Update {job_title} Job
+      </h2>
 
-            <form className="p-4 bg-base-200">
-               <div className='flex md:flex-row flex-col gap-4 mb-2'>
-
-               </div>
-            </form>
+      <form className="p-4 bg-base-200">
+        <div className="flex md:flex-row flex-col gap-4 mb-2">
+          {/* input for company name */}
+          <div className="form-control md:w-1/2 w-full">
+            <label className="label">
+              <span className="label-text font-bold">Company Name:</span>
+            </label>
+            <input
+              defaultValue={company_name}
+              type="text"
+              placeholder="Company Name"
+              name="company_name"
+              className="input input-bordered"
+              required
+            />
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
-export default UpdateJobPage
+export default UpdateJobPage;
