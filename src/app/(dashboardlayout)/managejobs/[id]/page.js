3dd072be8d9186bed7b77be4f52d4c87ea58a07f
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-29 10:58:33 +0600

    fetched necessary data for updating a job

--- src/app/(dashboardlayout)/managejobs/[id]/page.js
+++ src/app/(dashboardlayout)/managejobs/[id]/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import axios from 'axios';
 
 const UpdateJobPage = ({params})=>{
 
@@ -8,12 +9,27 @@ const UpdateJobPage = ({params})=>{
     axios.get(`http://localhost:5000/job/${params?.id}`)
     .then(res => setJob(res.data))
 
-    
+    const {
+        company_name,
+        job_title,
+        job_description,
+        location,
+        experience,
+        qualifications,
+        salary_range,
+        _id,
+        deadline,
+        category,
+        onsite_or_remote,
+        job_type,
+        employer_email,
+        job_post
+      } = job;
 
     return (
         <div className='py-5'>
             <h2 className="text-3xl text-center font-bold mb-2 text-[#033f63]">
-        Update Job 
+            Update {job_title} Job 
             </h2>
         </div>
     );
