a02b28c3a6423ef512ee8f8a3007f010f7e67776
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-05 18:22:47 +0600

    trying to implement react tabs

--- src/Components/HotJobs/HotJobs.jsx
+++ src/Components/HotJobs/HotJobs.jsx
@@ -1,24 +1,44 @@
+'use client';
 import JobCard from "../JobCard/JobCard";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import {useState, useEffect} from 'react';
 
-const HotJobs = async() => {
+const HotJobs =  () => {
+  const [hotJobs, setHotjobs] = useState([]);
 
-    const res = await fetch("http://localhost:5000/hotjobs?status=hot");
-    const hotJobs = await res.json();
-    
-
+  useEffect(()=>{
+    fetch("http://localhost:5000/hotjobs?status=hot")
+    .then(res => res.json())
+    .then(data => setHotjobs(data))
+  }, [])
 
   return (
     <div className="py-10">
       <h1 className="text-5xl text-[#033f63] font-bold text-center my-4">
         Hot Jobs: {hotJobs.length}
       </h1>
-        <p className='text-center text-2xl mb-4'>Search your favourite jobs from here.</p>
+      <p className="text-center text-2xl mb-4">
+        Search your favourite jobs from here.
+      </p>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4'>
-            {
-                hotJobs.map(job => <JobCard job={job} key={job._id}></JobCard>)
-            }
-        </div>
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4">
+        {hotJobs.map((job) => (
+          <JobCard job={job} key={job._id}></JobCard>
+        ))}
+      </div>
     </div>
   );
 };
