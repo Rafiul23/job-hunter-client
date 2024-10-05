9932a21b41a3636e94d68d0ab6725c803bb35591
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-05 22:56:23 +0600

    implemented tab system in jobs by category page

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -1,31 +1,70 @@
-'use client';
+"use client";
 import Image from "next/image";
 import corporateImage from "../../assets/globalisation-1014524_1280.png";
 import JobCard from "@/Components/JobCard/JobCard";
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Jobs = ({params, searchParams}) => {
-
+const Jobs = ({ params, searchParams }) => {
+  const [jobsByCategory, setJobsByCategory] = useState([]);
   const [displayJobs, setDisplayJobs] = useState([]);
   const [active, setActive] = useState(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(`http://localhost:5000/jobs?category=${searchParams?.category}`)
-    .then(res=> setDisplayJobs(res.data))
-  }, [searchParams])
+    .then((res) => {
+      setJobsByCategory(res.data)
+      setDisplayJobs(res.data);
+    });
+  }, [searchParams]);
 
-  const TabArray = ['All Jobs', 'On Site', 'Remote', 'Hybrid', 'Full Time', 'Part Time'];
 
-  const handleClick = (tab, index) =>{
+
+  const TabArray = [
+    "All Jobs",
+    "On Site",
+    "Remote",
+    "Hybrid",
+    "Full Time",
+    "Part Time",
+  ];
+
+  const handleClick = (tab, index) => {
     setActive(index);
-  }
+    handleTabOption(tab);
+  };
 
-  
+  const onsite = jobsByCategory.filter(
+    (job) => job.onsite_or_remote === "Onsite"
+  );
+  const remote = jobsByCategory.filter(
+    (job) => job.onsite_or_remote === "Remote"
+  );
+  const hybrid = jobsByCategory.filter(
+    (job) => job.onsite_or_remote === "Hybrid"
+  );
+  const fullTime = jobsByCategory.filter((job) => job.job_type === "Full Time");
+  const partTime = jobsByCategory.filter((job) => job.job_type === "Part Time");
+
+  const handleTabOption = (tab= 'All Jobs') => {
+    if(tab === 'All Jobs'){
+      setDisplayJobs(jobsByCategory);
+    } else if (tab === "On Site") {
+      setDisplayJobs(onsite);
+    } else if (tab === "Remote") {
+      setDisplayJobs(remote);
+    } else if (tab === "Hybrid") {
+      setDisplayJobs(hybrid);
+    } else if (tab === "Full Time") {
+      setDisplayJobs(fullTime);
+    } else if (tab === "Part Time") {
+      setDisplayJobs(partTime);
+    } 
+  };
 
   return (
     <div className="py-10">
-        <Image
+      <Image
         src={corporateImage}
         className="w-full h-[250px]"
         width={1200}
@@ -34,19 +73,31 @@ const Jobs = ({params, searchParams}) => {
       <h1 className="text-5xl text-[#033f63] font-bold text-center my-4">
         Job Category:{searchParams.category}
       </h1>
-      <p className='py-5 text-center text-2xl'><span className='font-bold'>{displayJobs?.length}</span> jobs found under <span className='font-bold'>{searchParams.category} </span> category</p>
+      <p className="py-5 text-center text-2xl">
+        <span className="font-bold">{displayJobs?.length}</span> jobs found
+        under <span className="font-bold">{searchParams.category} </span>{" "}
+        category
+      </p>
 
-      <div className='flex flex-wrap gap-4'>
-      {
-        TabArray.map((tab, index)=> <button className={`btn ${active === index ? 'bg-[#033f63] text-white' : ''}`} onClick={()=> handleClick(tab, index)}>{tab}</button>)
-      }
+      <div className="flex flex-wrap gap-4">
+        {TabArray.map((tab, index) => (
+          <button
+            key={index}
+            className={`btn ${
+              active === index ? "bg-[#033f63] text-white" : ""
+            }`}
+            onClick={() => handleClick(tab, index)}
+          >
+            {tab}
+          </button>
+        ))}
       </div>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4'>
-            {
-                displayJobs.map(job => <JobCard job={job} key={job._id}></JobCard>)
-            }
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4">
+        {displayJobs.map((job) => (
+          <JobCard job={job} key={job._id}></JobCard>
+        ))}
+      </div>
     </div>
   );
 };
