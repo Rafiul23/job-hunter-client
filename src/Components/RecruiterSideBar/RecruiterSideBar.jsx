b844d3aa4f4c279b649df46ad6e6ca919af742b0
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-13 19:10:16 +0600

    recruiter sidebar is changed

--- src/Components/RecruiterSideBar/RecruiterSideBar.jsx
+++ src/Components/RecruiterSideBar/RecruiterSideBar.jsx
@@ -1,52 +1,36 @@
 "use client";
+import useHandleSignOut from "@/hooks/useHandleSignOut";
+import useAdmin from "@/hooks/useAdmin";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import Swal from "sweetalert2";
-import axios from "axios";
-import useUser from "@/hooks/useUser";
 
 const RecruiterSideBar = () => {
-  const router = useRouter();
+  const handleSignOut = useHandleSignOut();
+  const {isRecruiter, loading} = useAdmin();
 
-  const { user } = useUser();
-  const { email } = user;
-  const handleSignOut = () => {
-    axios.post("http://localhost:5000/logout", email, {
-        withCredentials: true,
-      })
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
-    signOut()
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-
-    router.push("/");
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: "User logout successfully",
-      showConfirmButton: false,
-      timer: 1500,
-    });
-  };
-  return (
-    <div className="col-span-1 bg-[#033f63] h-screen w-full text-center text-white">
-      <div className="grid grid-cols-1 gap-5 ">
-        <hr />
-        <Link href="/recruiterprofile">
-          <button>Recruiter Profile</button>
-        </Link>
-        <hr />
-        <Link href="/myjobs">
-          <button>My Jobs</button>
-        </Link>
-        <hr />
-        <button onClick={handleSignOut}>Log Out</button>
-        <hr />
+  if(loading){
+    return <progress className="progress progress-success w-56"></progress>;
+  } else if(!isRecruiter){
+    return handleSignOut();
+  } else {
+    return (
+      <div className="col-span-1 bg-[#033f63] h-screen w-full text-center text-white">
+        <div className="grid grid-cols-1 gap-5 ">
+          <hr />
+          <Link href="/recruiterprofile">
+            <button>Recruiter Profile</button>
+          </Link>
+          <hr />
+          <Link href="/myjobs">
+            <button>My Jobs</button>
+          </Link>
+          <hr />
+          <button onClick={handleSignOut}>Log Out</button>
+          <hr />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 export default RecruiterSideBar;
