86a0216ab5dc2c3e985d3b12ac1d6b6139091bc7
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-10-17 23:57:39 +0600

    removed aos from cover image

--- src/Components/CoverImage/CoverImage.jsx
+++ src/Components/CoverImage/CoverImage.jsx
@@ -1,21 +1,10 @@
-"use client";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 import Image from "next/image";
 import corporateImage from "@/assets/globalisation-1014524_1280.png";
 
 const CoverImage = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
-
+ 
   return (
     <Image
-      data-aos="flip-down"
       src={corporateImage}
       className="w-full h-[250px]"
       width={1200}
